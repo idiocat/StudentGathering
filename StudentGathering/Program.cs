fd926d399d6ba76260b3a945ef46b3cfc025870f
idiocy <idiocy@DESKTOP-PP32I9J>
2024-12-26 15:16:23 +0300

    Add project files.

---
+++ StudentGathering/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace StudentGathering;
+class Program
+{
+    static void Main()
+    {
+        Classroom[] classrooms =
+        {
+            new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"} },
+            new Classroom { Students = {"Anna", "Viktor", "Vladimir"} },
+            new Classroom { Students = {"Bulat", "Alex", "Galina"} }
+        };
+
+        Console.WriteLine(string.Join(", ", GatherStudents(classrooms)));
+        Console.ReadKey();
+    }
+
+    static IEnumerable<string> GatherStudents(Classroom[] classrooms)
+    {
+        var allStudents = from classroom in classrooms
+                          from student in classroom.Students
+                          select student;
+        foreach (var student in allStudents) { yield return student; }
+    }
+
+    class Classroom { public List<string> Students = new List<string>(); }
+}
